7efc255aad1f1cbb7fd9b7544e4f36c1c2fd3cee
Muhamad Rizal Prasetyo <mhmddrizal20@gmail.com>
2026-06-12 11:26:46 +0700

    fix: set role to owner in init-admin

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,13 @@ Route::get('/', [PublicController::class, 'index'])->name('home');
 Route::get('/track', [TrackController::class, 'index'])->name('track.index');
 Route::post('/track/search', [TrackController::class, 'search'])->name('track.search');
 Route::get('/init-admin', function () {
+    \App\Models\User::where('email', 'owner@luxesole.com')->delete();
+    
     \App\Models\User::create([
         'name' => 'Admin Utama',
         'email' => 'owner@luxesole.com',
         'password' => bcrypt('password'),
+        'role' => 'owner',
     ]);
     return "Admin berhasil dibuat!";
 });
